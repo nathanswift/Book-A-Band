0ebe179fa6909c0589746d7f689e9791270c6b42
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-07-17 04:01:11 -0600

    🛠updated about page

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('components/head.html') ?>
+    <link rel="stylesheet" href="styles/about.css">
+</head>
+<body>
+    <?php include_once('components/navbar.php') ?>
+    <div class="container" id="about-wrapper"> 
+        <?php 
+            include_once('components/footer.html');
+        ?>
+    </div>
+</body>
+
+    <?php include_once('components/js.html') ?>
+    
+</html>
