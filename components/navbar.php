09e643f49c8c6e7288b52f776b8ca88ee14918a7
Jade Allen Cook <jadeallencook@gmail.com>
2018-07-17 04:08:52 -0600

    🔧 Updated navbar links

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Book A Band</a>
+  <a class="navbar-brand" href="index.php">Book A Band</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
     aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,13 +8,13 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Bands</a>
+        <a class="nav-link" href="browse.php">Bands</a>
       </li>
       <li class="nav-item">
           <a class="nav-link" href="about.php">About</a>
         </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+        <a class="nav-link dropdown-toggle" href="browse.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
           aria-expanded="false">
           Genres
         </a>
@@ -23,7 +23,7 @@
             include_once('etc/genres.php');
             foreach($genres as $key=>$value):
           ?>
-          <a class="dropdown-item" href="#"><?php echo $value; ?></a>
+          <a class="dropdown-item" href="browse.php?genre=<?php echo $value; ?>"><?php echo $value; ?></a>
           <?php endforeach; ?>
         </div>
       </li>
